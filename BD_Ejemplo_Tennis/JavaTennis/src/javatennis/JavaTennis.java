e838f329a9d17e9f916c930da8c33a2fce5781da
Harol003@gmail.com <harol003@gmail.com>
2025-06-04 16:55:55 -0500

    04062025

---
+++ BD_Ejemplo_Tennis/JavaTennis/src/javatennis/JavaTennis.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javatennis;
+
+/**
+ *
+ * @author Harol
+ */
+public class JavaTennis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Tennis Menu = new Tennis();
+
+        // Mostrar la vista
+        Menu.setVisible(true);
+        
+        Menu.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
